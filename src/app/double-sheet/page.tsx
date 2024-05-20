b371b28c553e4afbe480feed7a9d05c116fd4f29
류정우 <evencoding@gmail.com>
2024-05-20 13:48:05 +0900

    feat: double-sheet

---
+++ src/app/double-sheet/page.tsx
@@ -0,0 +1,10 @@
+import DoubleSheet from '@/components/double-sheet'
+
+export default function DoubleSheetPage() {
+  return (
+    <div>
+      <h1 className="center text-lg font-bold mb-[20px]">이중 Sheet</h1>
+      <DoubleSheet />
+    </div>
+  )
+}
---
+++ src/components/double-sheet.tsx
@@ -0,0 +1,41 @@
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+
+export default function DoubleSheet() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <div className="flex items-center fixed bottom-0 max-w-[760px] w-full p-[20px] justify-between bg-gray-400 rounded-t-[12px] cursor-pointer">
+          <div>
+            <div className="flex items-end gap-[8px]">
+              <div className="text-xl font-bold">판교의 집</div>
+              <div className="text-sm">서울시 광진구 자양동</div>
+            </div>
+            <div>5.0 ⭐️ ⭐️ ⭐️ ⭐️ ⭐️</div>
+            <div># 방음 좋은</div>
+          </div>
+          <div className="size-[80px] bg-gray-200" />
+        </div>
+      </SheetTrigger>
+      <SheetContent side="bottom" className="rounded-t-[12px] min-h-[600px] p-0">
+        <Sheet>
+          <SheetTrigger asChild>
+            <div className="flex items-center fixed bottom-0 w-full px-[20px] justify-between bg-gray-400 rounded-t-[12px] cursor-pointer min-h-[600px]">
+              <div>
+                <div className="flex items-end gap-[8px]">
+                  <div className="text-xl font-bold">판교의 집</div>
+                  <div className="text-sm">서울시 광진구 자양동</div>
+                </div>
+                <div>5.0 ⭐️ ⭐️ ⭐️ ⭐️ ⭐️</div>
+                <div># 방음 좋은</div>
+              </div>
+              <div className="size-[80px] bg-gray-200" />
+            </div>
+          </SheetTrigger>
+          <SheetContent side="bottom" className="rounded-t-[12px] min-h-screen">
+            ok
+          </SheetContent>
+        </Sheet>
+      </SheetContent>
+    </Sheet>
+  )
+}
